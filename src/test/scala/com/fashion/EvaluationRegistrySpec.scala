efb4558bb4f49a75da40489fa0376a651b65d4ae
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2021-09-10 10:56:20 +0200

    test toSpeeches()

--- src/test/scala/com/fashion/EvaluationRegistrySpec.scala
+++ src/test/scala/com/fashion/EvaluationRegistrySpec.scala
@@ -1,12 +1,17 @@
 package com.fashion
 
-import org.scalatest.wordspec.AnyWordSpec
 import com.fashion.EvaluationRegistry._
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class EvaluationRegistrySpec extends AnyWordSpec with Matchers {
 
   "EvaluationRegistry" should {
+
+    // tests for asCsvArray()
     "create empty csv for empty input" in {
       // empty input
       val input ="";
@@ -43,6 +48,49 @@ class EvaluationRegistrySpec extends AnyWordSpec with Matchers {
       // assert
       val expectedResult = Array(Array("a", "b", "c"), Array("1", "2", "3"))
       result should ===(expectedResult)
+    }
+
+    // tests for toSpeeches()
+    "create empty speeches with input Array(Array())" in {
+      val input: Array[Array[String]] = Array(Array());
+
+      // test
+      val result = toSpeeches(input)
+
+      //assert
+      result should ===(Array())
+    }
+
+    """create empty speeches with input Array(Array(""))""" in {
+      val input: Array[Array[String]] = Array(Array(""));
+
+      // test
+      val result = toSpeeches(input)
+
+      //assert
+      result should ===(Array())
+    }
+
+    """create speeches with one speech""" in {
+      // data with one speech
+      val input: Array[Array[String]] = Array(Array(HEADER_SPEAKER, HEADER_TOPIC, HEADER_DATE, HEADER_WORDS_COUNT), Array("a speaker", "a topic", "2019-12-21", "123"))
+
+      // test
+      val result = toSpeeches(input)
+
+      //assert
+      result should ===(Array(Speech("a speaker", "a topic", new SimpleDateFormat("yyyy-MM-dd").parse("2019-12-21"), 123)))
+    }
+
+    """create speeches with different column order in input""" in {
+      // flip topic being in first column and speaker in second row
+      val input: Array[Array[String]] = Array(Array(HEADER_TOPIC, HEADER_SPEAKER, HEADER_DATE, HEADER_WORDS_COUNT), Array("a topic", "a speaker", "2019-12-21", "123"))
+
+      // test
+      val result = toSpeeches(input)
+
+      //assert
+      result should ===(Array(Speech("a speaker", "a topic", new SimpleDateFormat("yyyy-MM-dd").parse("2019-12-21"), 123)))
     }
   }
 
