69929e8fb2852eb2341a3183d8a59a30d173e1a4
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2021-09-13 11:59:47 +0200

    5s timeout for tests

--- src/test/scala/com/fashion/EvaluationRoutesSpec.scala
+++ src/test/scala/com/fashion/EvaluationRoutesSpec.scala
@@ -1,12 +1,16 @@
 package com.fashion
 
+import akka.actor.ActorSystem
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.testkit.RouteTestTimeout
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+
+import scala.concurrent.duration._
 
 class EvaluationRoutesSpec extends AnyWordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
   // the Akka HTTP route testkit does not yet support a typed actor system (https://github.com/akka/akka-http/issues/2036)
@@ -26,6 +30,8 @@ class EvaluationRoutesSpec extends AnyWordSpec with Matchers with ScalaFutures w
   // use the json formats to marshal and unmarshall objects in the test
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import JsonFormats._
+
+  implicit def default(implicit system: ActorSystem) = RouteTestTimeout(5.seconds)
 
   "EvaluationRoutes" should {
     "evaluate speeches" in {
